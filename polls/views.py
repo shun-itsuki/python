a023b9efc7a7e68d883fbd5cf3cb1ae2df5c6bdc
shun-itsuki <it_shun_1125@yahoo.co.jp>
2021-03-26 00:03:04 +0900

    second

--- polls/views.py
+++ polls/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import HttpResponse
 
 # Create your views here.
 
 def index(request):
-    return HttpRedponse("Hello world this is index page")
+    return HttpResponse("Hello world this is index page")
